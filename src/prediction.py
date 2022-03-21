8d93c517ddcbd2000a69e3c25ce32a9922eb864d
Esra Ozikinci <esraozikinci@gmail.com>
2022-03-21 17:37:02 +0000

    Upload New File

---
+++ src/prediction.py
@@ -0,0 +1,33 @@
+import argparse
+
+import numpy as np
+import pandas as pd
+
+from src.prep_and_model_training import ClassifierModel
+
+parser = argparse.ArgumentParser(description="Process for building model.")
+parser.add_argument("-i",
+                    "--prediction_file",
+                    dest="prediction_file",
+                    required=False,
+                    default="../raw_data/sample_prediction_data.csv",
+                    help="Input file path")
+args = parser.parse_args()
+prediction_file_path = args.prediction_file
+df_predict = pd.read_csv(prediction_file_path, sep=",")
+
+features = ["total_count_computer_phone_tablet", "total_count", "tablet_count", "smarthome_count",
+            "speed_tier_bucket", "interference_5G_bucket", "WEM_bucket", "bad_quality_traffic_tx",
+            "bad_quality_traffic_rx", "bad_quality_speed_tier", "bad_quality_interference_5G",
+            "bad_quality_WEM"]
+df_predict = ClassifierModel().create_new_features(df_predict)
+model_path = "../model/trained_data.pkl"
+xgbr_model = ClassifierModel().read_pickle(model_path)
+
+if "has_pod" in df_predict.columns:
+    df_predict = df_predict.drop(columns="has_pod")
+X = ClassifierModel().feature_scaling("predict", df_predict, columns=features)
+df_predict.loc[:, "pred_pod"] = xgbr_model.predict(X[features])
+df_predict.loc[:, "pred_pod"] = np.rint(df_predict["pred_pod"])
+pred_prob_xgbr = xgbr_model.predict_proba(X[features])
+print(df_predict[df_predict["pred_pod"] > 0]["gw_mac_address"].values)
